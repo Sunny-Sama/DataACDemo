834bcc7d0e7411add4a17bcfce5fc835cef75038
Sunny-Sama <13302010096@fudan.edu.cn>
2017-01-15 19:42:33 +0800

    Web Page Structure

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Sunny on 2017/1/15.
+ */
+var myApp = angular.module('myApp', ['ui.router', 'myApp.controllers', 'accessManage.controllers']);
+myApp.config(['$stateProvider', '$urlRouterProvider', function ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.when('','/main');
+  $stateProvider
+      .state('main', {
+          url: '/main',
+          cache: 'false',
+          templateUrl: 'templates/main.html',
+          controller: 'mainCtrl'
+      })
+
+      .state('accessManage', {
+          url: '/accessManage',
+          abstract: true,
+          templateUrl: 'templates/access-manage.html'
+      })
+
+      .state('accessManage.userAC', {
+          url: '',
+          cache: 'false',
+          templateUrl: 'templates/user-ac.html',
+          controller: 'userACCtrl'
+      })
+
+      .state('accessManage.attributeAC', {
+          url: '/attributeAC',
+          cache: 'false',
+          templateUrl: 'templates/attribute-ac.html',
+          controller: 'attributeACCtrl'
+      })
+}]);
---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Sunny on 2017/1/15.
+ */
+angular.module('myApp.controllers', [])
+
+.controller('mainCtrl', function(){
+
+});
+
+angular.module('accessManage.controllers', [])
+
+.controller('userACCtrl', function(){
+
+})
+
+.controller('attributeAC', function(){
+
+});
